d202e42a4ad4cf7407c315d0226ac9756d9c3819
MoffuGit <moffu.by@gmail.com>
2025-10-11 22:41:47 -0600

    Something change

---
+++ ares/ares/AresKit.xcframework/macos-arm64/Headers/ares.h
@@ -0,0 +1,20 @@
+#ifndef ZIG_COUNTER_H
+#define ZIG_COUNTER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h> // For int32_t
+
+void zig_increment_counter(void);
+void zig_decrement_counter(void);
+int32_t zig_get_counter(void);
+void zig_init_counter(void);
+void zig_deinit_counter(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ZIG_COUNTER_H
