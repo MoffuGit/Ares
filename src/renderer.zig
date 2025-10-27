3e41a4c10abf2647cc24f4527a5d1c4284ce20a7
MoffuGit <moffu.by@gmail.com>
2025-10-26 23:21:54 -0600

    WIP: background change

--- src/renderer.zig
+++ src/renderer.zig
@@ -14,6 +14,7 @@ pub const Renderer = struct {
     size: size,
     api: Metal,
     target: Target,
+    frame_count: usize = 0,
 
     pub fn init(alloc: Allocator, opts: Options) !Renderer {
         var api = try Metal.init(opts.rt_surface);
@@ -42,15 +43,73 @@ pub const Renderer = struct {
 
         self.size = .{ .height = surface_size.height, .width = surface_size.width };
 
+        if (self.size.width > 0 and self.size.height > 0) {
+            const target = try self.api.initTarget(self.size.width, self.size.height);
+            errdefer target.deinit();
+            self.target.deinit();
+            self.target = target;
+        }
+
         var frame_ctx = try self.api.beginFrame(self, &self.target);
         defer frame_ctx.complete(sync);
+
+        self.frame_count += 1;
 
         var render_pass = frame_ctx.renderPass(&.{
             .{
                 .target = .{
                     .target = self.target,
                 },
-                .clear_color = .{ 0.0, 0.0, 1.0, 1.0 }, // Red color
+                .clear_color = blk: {
+                    var r: f32 = 0.0;
+                    var g: f32 = 0.0;
+                    var b: f32 = 0.0;
+                    const segment_size: usize = 256;
+                    const total_segments: usize = 6;
+                    const cycle_length = segment_size * total_segments;
+                    const current_step = self.frame_count % cycle_length;
+                    const segment_idx = current_step / segment_size; // Which segment are we in (0-5)
+                    // Value from 0.0 to 1.0 within the current segment
+                    const segment_offset = @as(f32, @floatFromInt(current_step % segment_size)) / @as(f32, segment_size - 1);
+                    switch (segment_idx) {
+                        0 => { // Red to Yellow (R=1, G=increasing, B=0)
+                            r = 1.0;
+                            g = segment_offset;
+                            b = 0.0;
+                        },
+                        1 => { // Yellow to Green (R=decreasing, G=1, B=0)
+                            r = 1.0 - segment_offset;
+                            g = 1.0;
+                            b = 0.0;
+                        },
+                        2 => { // Green to Cyan (R=0, G=1, B=increasing)
+                            r = 0.0;
+                            g = 1.0;
+                            b = segment_offset;
+                        },
+                        3 => { // Cyan to Blue (R=0, G=decreasing, B=1)
+                            r = 0.0;
+                            g = 1.0 - segment_offset;
+                            b = 1.0;
+                        },
+                        4 => { // Blue to Magenta (R=increasing, G=0, B=1)
+                            r = segment_offset;
+                            g = 0.0;
+                            b = 1.0;
+                        },
+                        5 => { // Magenta to Red (R=1, G=0, B=decreasing)
+                            r = 1.0;
+                            g = 0.0;
+                            b = 1.0 - segment_offset;
+                        },
+                        else => { // Should not happen with modulo
+                            r = 0.0;
+                            g = 0.0;
+                            b = 0.0; // Default to black
+                        },
+                    }
+                    break :blk .{ r, g, b, 1.0 };
+                },
             },
         });
         render_pass.complete();
