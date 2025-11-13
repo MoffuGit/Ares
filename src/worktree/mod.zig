792a7e4114baa931d1153ac8e30ccc2e2304d8b2
MoffuGit <moffu.by@gmail.com>
2025-11-12 22:19:53 -0600

    WIP: Added Worktree

---
+++ src/worktree/mod.zig
@@ -0,0 +1,5 @@
+//NOTE:
+//in here it will go all the referent to the worktree, once i have the directory watchor on libxev
+//i will read over the zed impl for a worktree
+//The already handle a lot, like, git ignore and different setting you can have,
+//i will copy the most imporant part for now, like, how to struct the tree itself, with folders and files
