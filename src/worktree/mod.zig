494514f39c571e2940783e35f4dfda0e72eafc61
MoffuGit <moffu.by@gmail.com>
2025-11-13 19:12:17 -0600

    WIP: The libxev patch wasn't finish

--- src/worktree/mod.zig
+++ src/worktree/mod.zig
@@ -1,5 +1,1 @@
-//NOTE:
-//in here it will go all the referent to the worktree, once i have the directory watchor on libxev
-//i will read over the zed impl for a worktree
-//The already handle a lot, like, git ignore and different setting you can have,
-//i will copy the most imporant part for now, like, how to struct the tree itself, with folders and files
+const xev = @import("../global.zig").xev;
