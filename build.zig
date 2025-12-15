cf2b73f8a66af02348883c976643f3f3bc2ae327
MoffuGit <moffu.by@gmail.com>
2025-12-15 11:35:14 -0600

    WIP: test datastruct

--- build.zig
+++ build.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const XCFrameworkStep = @import("src/build/CXFramerworkStep.zig");
 const MetallibStep = @import("src/build/MetallibStep.zig");
+const Step = std.Build.Step;
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -17,6 +18,35 @@ pub fn build(b: *std.Build) void {
     const xev = b.dependency("libxev", .{ .target = target, .optimize = optimize });
 
     exe.root_module.addImport("xev", xev.module("xev"));
+
+    const test_exe: *Step.Compile = test_exe: {
+        const test_filter = b.option(
+            []const u8,
+            "test-filter",
+            "Filter for test",
+        );
+        const test_exe = b.addTest(.{
+            .name = "ares-test",
+            .filters = if (test_filter) |filter| &.{filter} else &.{},
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/main.zig"),
+                .target = target,
+                .optimize = optimize,
+            }),
+        });
+        switch (target.result.os.tag) {
+            .linux, .macos => test_exe.linkLibC(),
+            else => {},
+        }
+        break :test_exe test_exe;
+    };
+
+    // "test" Step
+    {
+        const tests_run = b.addRunArtifact(test_exe);
+        const test_step = b.step("test", "Run tests");
+        test_step.dependOn(&tests_run.step);
+    }
 
     const lib = b.addLibrary(.{
         .name = "Ares",
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
 const std = @import("std");
+const datastruct = @import("datastruct/mod.zig");
 
 pub fn main() !void {
     std.debug.print("Hello, World!\n", .{});
+}
+
+test {
+    _ = datastruct;
 }
