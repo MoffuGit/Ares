286a1f34186034fb8b98d357f5d42cae0927b102
MoffuGit <moffu.by@gmail.com>
2025-12-16 23:56:58 -0600

    WIP: i didn't ahve much time

--- src/datastruct/b_plus_tree.zig
+++ src/datastruct/b_plus_tree.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
+const panic = std.debug.panic;
 
 const BASE: usize = 6;
 
@@ -8,24 +9,71 @@ const BASE: usize = 6;
 //but i need to keep reading over the zed sumtree
 //and teh zig b plus tree repo
 //with that two things i can write my own b plus tree
+//
+pub const Error = error{
+    OutOfMemory,
+    DuplicateKey,
+} || Allocator.Error;
 
-pub fn NodeType(comptime K: type, comptime V: type) type {
+pub fn NodeType(comptime K: type, comptime V: type, comp: *const fn (a: K, b: K) std.math.Order) type {
     return union(enum) {
         const Self = @This();
 
-        Internal: struct { edge: [2 * BASE]*Self = undefined, keys: [2 * BASE - 1]K = undefined, len: u16 = 0 },
+        Internal: struct { childs: [2 * BASE]*Self = undefined, keys: [2 * BASE - 1]K = undefined, len: u16 = 0 },
         Leaf: struct { items: [2 * BASE - 1]V = undefined, keys: [2 * BASE - 1]K = undefined, len: u16 = 0, next: ?*Self = null },
-        //is_empty
-        //is_leaf
-        //items
-        //panic when a call is for a wrogn type of Node
-        //etc...
+
+        pub fn add_item(self: *Self, key: K, value: V) Error!void {
+            switch (self.*) {
+                .Internal => panic("items can be only added to leaf nodes", .{}),
+                .Leaf => {
+                    const leaf = &self.*.Leaf;
+                    if (leaf.len == leaf.items.len) {
+                        return error.OutOfMemory;
+                    }
+
+                    var idx: u16 = leaf.len;
+
+                    while (idx > 0) : (idx -= 1) {
+                        switch (comp(key, leaf.keys[idx - 1])) {
+                            .gt => break,
+                            .lt => {
+                                leaf.keys[idx] = leaf.keys[idx - 1];
+                                leaf.items[idx] = leaf.items[idx - 1];
+                            },
+                            .eq => return error.DuplicateKey,
+                        }
+                    }
+
+                    leaf.keys[idx] = key;
+                    leaf.items[idx] = value;
+                    leaf.len += 1;
+                },
+            }
+        }
+        pub fn is_empty(self: *Self) bool {
+            switch (self.*) {
+                .Internal => {
+                    const internal = &self.*.Internal;
+                    return internal.len == 0;
+                },
+                .Leaf => {
+                    const leaf = &self.*.Leaf;
+                    return leaf.len == 0;
+                },
+            }
+        }
+
+        pub fn is_leaf(self: *Self) bool {
+            switch (self.*) {
+                .Internal => return false,
+                .Leaf => return true,
+            }
+        }
     };
 }
 
-pub fn BPlusTree(comptime K: type, comptime V: type, comptime comp: *const fn (a: *K, b: *K) std.math.Order) type {
-    _ = comp;
-    const Node = NodeType(K, V);
+pub fn BPlusTree(comptime K: type, comptime V: type, comptime comp: *const fn (a: K, b: K) std.math.Order) type {
+    const Node = NodeType(K, V, comp);
 
     return struct {
         const Self = @This();
@@ -44,88 +92,50 @@ pub fn BPlusTree(comptime K: type, comptime V: type, comptime comp: *const fn (a
             self.alloc.destroy(self.root);
         }
 
-        pub fn push(self: *Self, key: K, value: V) !void {
+        pub fn push(self: *Self, key: K, value: V) Error!void {
             const node = try self.alloc.create(Node);
+            defer self.alloc.destroy(node);
+
             node.* = .{ .Leaf = .{} };
-            //now i needc to add the key and the value inside the buffers
-            //update the length
-            //and call append
+
+            assert(node.is_empty());
+
+            node.add_item(key, value) catch {};
+
+            try self.append(self.root, node);
         }
 
-        //     pub fn push(&mut self, item: T, cx: <T::Summary as Summary>::Context<'_>) {
-        //     let summary = item.summary(cx);
-        //     self.append(
-        //         SumTree(Arc::new(Node::Leaf {
-        //             summary: summary.clone(),
-        //             items: ArrayVec::from_iter(Some(item)),
-        //             item_summaries: ArrayVec::from_iter(Some(summary)),
-        //         })),
-        //         cx,
-        //     );
-        // }
-
-        //append:
-        //  (self, nodeA, nodeB)
-        //  if nodeA is empty
-        //  root = nodeB
-        //  alloc.destroy(nodeA)
-        //
-        //  else
-        //  if height of nodeA is less thatn the height from nodeB
-        //  remove childrens from nodeB until the height is the same
-        //
-        //  else
-        //  the height is equal or less than NodeA
-        //  try to push_recursive, this will split the nodes and and borrow and merge
-        //  this will set on NodeA one side of the tree, on the nodeB another side of the tree
-        //  latter they will get join and after that you can set NodeA as the result Node of the
-        //  join, you need to remember when to destroy
-        //
-        //  try to remove subnodes from nodeB until the height is equal to the current height of the tree
-        //  and try to append this ones
-        //
-        //     if self.is_empty() {
-        //         *self = other;
-        //     } else if !other.0.is_leaf() || !other.0.items().is_empty() {
-        //         if self.0.height() < other.0.height() {
-        //             for tree in other.0.child_trees() {
-        //                 self.append(tree.clone(), cx);
-        //             }
-        //         } else if let Some(split_tree) = self.push_tree_recursive(other, cx) {
-        //             *self = Self::from_child_trees(self.clone(), split_tree, cx);
-        //         }
-        //     }
-        // }
-
-        //remember you are going to shift and unshift the values inside the buffers of the node
-        //this is done for adding and removing when there is no need of splitting
-        //push(self, k, t)
-        // create a Node
-        // and call
-        //append(self, node one, node two)
-        //...
-        //split
-        //merge
-        //borrow
-        //...
-        //join(self, other tree)
-        //...
-        //delete
-        //etc...:w
-        //
-        //replace root node for other node, when calling something like from_child_trees, root = new_tree
+        pub fn append(_: *Self, parent: *Node, child: *Node) Error!void {
+            if (parent.is_empty()) {
+                parent.* = child.*;
+            } else {
+                //add heigth to a node,
+                //check if the height of parent is less than the height of child
+                //if is true, get down one level of child and try to append that trees
+                //else
+                //try to push the child tree to the parent tree, for that you probably are going to need to split the tree
+                //and join it latter,
+                //one detail is that I'm not sure of how should i split the tree
+                //or how to ask for a sibling value, but that ok, i can read it from zed
+                //or from the other repo with the impl of a b + tree
+            }
+        }
     };
 }
 
-fn test_comp(a: *usize, b: *usize) std.math.Order {
-    return std.math.order(a.*, b.*);
+fn test_comp(a: usize, b: usize) std.math.Order {
+    return std.math.order(a, b);
 }
 
-test "init B+ tree" {
+test "init B+ tree and first push value" {
     const testing = std.testing;
     const alloc = testing.allocator;
 
     const T = BPlusTree(usize, usize, test_comp);
-    const tree = try T.init(alloc);
+    var tree = try T.init(alloc);
     defer tree.deinit();
+
+    try tree.push(0, 1);
+    assert(!tree.root.is_empty());
+    assert(tree.root.is_leaf());
 }
