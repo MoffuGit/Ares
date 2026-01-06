a4a966f8ea059390eff44d468eca997886b95f50
MoffuGit <moffu.by@gmail.com>
2026-01-06 14:22:37 -0600

    WIP

--- src/worktree/Message.zig
+++
@@ -1,6 +0,0 @@
-const xev = @import("../global.zig").xev;
-
-pub const Message = union(enum) {
-    fsevent: u32,
-    pwd: []u8,
-};
--- src/worktree/mod.zig
+++ src/worktree/mod.zig
@@ -1,1 +1,13 @@
 const Worktree = @This();
+//WARN:
+//the idea of this is to wakeup the scanner thread only when needed,
+//and the same for the monitor,
+//this two threads can speak one with each other
+//but only the scanner can update the worktree
+//
+//scanner: Scanner
+//scanner_thread: Thread
+//
+//monitor: Monitor,
+//monitor_thread: Thread
+//
