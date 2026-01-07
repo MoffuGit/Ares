d81192bc06be4b7caab7d969c46d5c2cda090251
MoffuGit <moffu.by@gmail.com>
2026-01-06 20:58:13 -0600

    WIP: Init and deinit worktree

--- src/worktree/mod.zig
+++ src/worktree/mod.zig
@@ -1,13 +1,64 @@
+const std = @import("std");
+const BPlusTree = @import("../datastruct/mod.zig").BPlusTree;
+const Monitor = @import("monitor/mod.zig");
+const MonitorThread = @import("monitor/Thread.zig");
+const Scanner = @import("scanner/mod.zig");
+const ScannerThread = @import("scanner/Thread.zig");
+const Allocator = std.mem.Allocator;
+
+const log = std.log.scoped(.worktree);
+
 const Worktree = @This();
-//WARN:
-//the idea of this is to wakeup the scanner thread only when needed,
-//and the same for the monitor,
-//this two threads can speak one with each other
-//but only the scanner can update the worktree
-//
-//scanner: Scanner
-//scanner_thread: Thread
-//
-//monitor: Monitor,
-//monitor_thread: Thread
-//
+
+alloc: Allocator,
+
+root: []u8 = undefined,
+entries: BPlusTree(usize, usize) = undefined,
+
+monitor: Monitor,
+monitor_thread: MonitorThread,
+monitor_thr: std.Thread,
+
+scanner: Scanner,
+scanner_thread: ScannerThread,
+scanner_thr: std.Thread,
+
+pub fn init(self: *Worktree, alloc: Allocator) !Worktree {
+    var monitor_thread = try MonitorThread.init(alloc, &self);
+    errdefer monitor_thread.deinit();
+
+    var scanner_thread = try ScannerThread.init(alloc, &self);
+    errdefer scanner_thread.deinit();
+
+    var monitor = try Monitor.init(alloc);
+    errdefer monitor.deinit();
+
+    var scanner = try Scanner.init(alloc);
+    errdefer scanner.deinit();
+
+    self.* = .{ .alloc = alloc, .root = undefined, .entries = undefined, .monitor = monitor, .monitor_thread = monitor_thread, .monitor_thr = undefined, .scanner = scanner, .scanner_thread = scanner_thread, .scanner_thr = undefined };
+
+    self.monitor_thr = try std.Thread.spawn(.{}, MonitorThread.Thread.threadMain, .{&self.monitor_thread});
+    self.scanner_thr = try std.Thread.spawn(.{}, ScannerThread.Thread.threadMain, .{&self.scanner_thread});
+}
+
+pub fn deinit(self: *Worktree) void {
+    {
+        self.monitor_thread.stop.notify() catch |err| {
+            log.err("error notifying monitor thread to stop, may stall err={}", .{err});
+        };
+        self.monitor_thr.join();
+    }
+
+    {
+        self.scanner_thread.stop.notify() catch |err| {
+            log.err("error notifying scanner thread to stop, may stall err={}", .{err});
+        };
+        self.scanner_thr.join();
+    }
+
+    self.monitor.deinit();
+    self.scanner.deinit();
+
+    log.info("Worktree closed", .{});
+}
