d0880ae54defe94f7a1d829858b8758ee9517066
MoffuGit <moffu.by@gmail.com>
2026-01-10 21:59:56 -0600

    WIP: added vaxis

--- build.zig
+++ build.zig
@@ -7,11 +7,21 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{ .name = "ares", .root_module = b.createModule(.{
+    const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
-    }) });
+    });
+
+    const vaxis = b.dependency("vaxis", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_mod.addImport("vaxis", vaxis.module("vaxis"));
+
+    const exe = b.addExecutable(.{ .name = "ares", .root_module = exe_mod });
+
     b.installArtifact(exe);
     b.default_step.dependOn(&exe.step);
 
