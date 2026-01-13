5e68cc2e5bfd9fdb916c4470b9c1faa7eda5adfd
MoffuGit <moffu.by@gmail.com>
2026-01-12 21:36:54 -0600

    WIP: new app

--- src/App.zig
+++ src/App.zig
@@ -1,53 +1,81 @@
+const std = @import("std");
+const vaxis = @import("vaxis");
+const builtin = @import("builtin");
+const posix = std.posix;
+
+const log = std.log.scoped(.app);
+
+const Allocator = std.mem.Allocator;
+
 const App = @This();
 
-const std = @import("std");
-const apprt = @import("apprt/embedded.zig");
-const Allocator = std.mem.Allocator;
-const SurfaceList = std.ArrayListUnmanaged(*apprt.Surface);
-const fontpkg = @import("font/mod.zig");
-const Grid = fontpkg.Grid;
-
 alloc: Allocator,
-surfaces: SurfaceList,
 
-pub fn create(alloc: Allocator) !*App {
-    var app = try alloc.create(App);
-    errdefer alloc.destroy(app);
+buffer: [1024]u8 = undefined,
 
-    try app.init(alloc);
+tty: vaxis.Tty,
+
+pub fn init(alloc: Allocator) !App {
+    var app: App = .{ .alloc = alloc, .tty = undefined };
+
+    var tty = try vaxis.Tty.init(&app.buffer);
+    errdefer tty.deinit();
+
+    app.tty = tty;
+
     return app;
 }
 
-pub fn init(self: *App, alloc: Allocator) !void {
-    self.* = .{
-        .alloc = alloc,
-        .surfaces = .{},
-    };
-}
-
 pub fn deinit(self: *App) void {
-    for (self.surfaces.items) |surface| surface.deinit();
-    self.surfaces.deinit(self.alloc);
+    self.tty.deinit();
 }
 
-pub fn destroy(self: *App) void {
-    self.deinit();
+pub fn run(self: *App) !void {
+    // const vx = &self.vx;
 
-    self.alloc.destroy(self);
-}
+    // try vx.enterAltScreen(tty.writer());
+    // try vx.queryTerminal(tty.writer(), 1 * std.time.ns_per_s);
+    // try vx.setBracketedPaste(tty.writer(), true);
+    // try vx.subscribeToColorSchemeUpdates(tty.writer());
+    // try vx.setMouseMode(tty.writer(), true);
 
-pub fn addSurface(self: *App, surface: *apprt.Surface) !void {
-    try self.surfaces.append(self.alloc, surface);
-}
+    // const winsize = try vaxis.Tty.getWinsize(self.tty.fd);
 
-pub fn deleteSurface(self: *App, surface: *apprt.Surface) void {
-    var i: usize = 0;
-    while (i < self.surfaces.items.len) {
-        if (self.surfaces.items[i] == surface) {
-            _ = self.surfaces.swapRemove(i);
-            continue;
+    var parser: vaxis.Parser = .{};
+
+    var buf: [1024]u8 = undefined;
+    var read_start: usize = 0;
+
+    var cache: vaxis.GraphemeCache = .{};
+
+    while (true) {
+        const n = self.tty.read(buf[read_start..]) catch |err| {
+            if (err == error.WouldBlock) continue else return err;
+        };
+        var seq_start: usize = 0;
+        while (seq_start < n) {
+            const result = try parser.parse(buf[seq_start..n], self.alloc);
+            if (result.n == 0) {
+                // copy the read to the beginning. We don't use memcpy because
+                // this could be overlapping, and it's also rare
+                const initial_start = seq_start;
+                while (seq_start < n) : (seq_start += 1) {
+                    buf[seq_start - initial_start] = buf[seq_start];
+                }
+                read_start = seq_start - initial_start + 1;
+                continue;
+            }
+            read_start = 0;
+            seq_start += result.n;
+
+            const event = result.event orelse continue;
+            try self.eventCallback(&cache, event);
         }
-
-        i += 1;
     }
+}
+
+fn eventCallback(self: *App, cache: *vaxis.GraphemeCache, event: vaxis.Event) !void {
+    _ = self;
+    _ = cache;
+    log.err("event: {}", .{event});
 }
--- src/main.zig
+++ src/main.zig
@@ -4,108 +4,19 @@ const datastruct = @import("datastruct/mod.zig");
 const vaxis = @import("vaxis");
 const Cell = vaxis.Cell;
 
-pub const panic = vaxis.panic_handler;
+const App = @import("App.zig");
 
-const Event = union(enum) {
-    key_press: vaxis.Key,
-    winsize: vaxis.Winsize,
-};
+const log = std.log.scoped(.main);
 
 const global = &@import("global.zig").state;
 
 pub fn main() !void {
-    try global.init();
-    defer global.deinit();
-
     const alloc = global.alloc;
 
-    var buffer: [1024]u8 = undefined;
-    var tty = try vaxis.Tty.init(&buffer);
-    defer tty.deinit();
+    var app = try App.init(alloc);
+    defer app.deinit();
 
-    var vx = try vaxis.init(alloc, .{});
-    defer vx.deinit(alloc, tty.writer());
-
-    var loop: vaxis.Loop(Event) = .{ .tty = &tty, .vaxis = &vx };
-    try loop.init();
-
-    try loop.start();
-    defer loop.stop();
-
-    try vx.enterAltScreen(tty.writer());
-    try vx.queryTerminal(tty.writer(), 1 * std.time.ms_per_s);
-
-    var pct: u8 = 0;
-    var dir: enum {
-        up,
-        down,
-    } = .up;
-
-    const fg = [_]u8{ 192, 202, 245, 255 };
-    const bg = [_]u8{ 0, 0, 0, 255 };
-
-    // block until we get a resize
-    while (true) {
-        const event = loop.nextEvent();
-        switch (event) {
-            .key_press => |key| if (key.matches('c', .{ .ctrl = true })) return,
-            .winsize => |ws| {
-                try vx.resize(alloc, tty.writer(), ws);
-                break;
-            },
-        }
-    }
-
-    const tick_ms: u64 = @divFloor(std.time.ms_per_s, 120);
-    var next_frame_ms: u64 = @intCast(std.time.milliTimestamp());
-
-    while (true) {
-        const now_ms: u64 = @intCast(std.time.milliTimestamp());
-        if (now_ms >= next_frame_ms) {
-            // Deadline exceeded. Schedule the next frame
-            next_frame_ms = now_ms + tick_ms;
-        } else {
-            // Sleep until the deadline
-            std.Thread.sleep((next_frame_ms - now_ms) * std.time.ns_per_ms);
-            next_frame_ms += tick_ms;
-        }
-
-        while (loop.tryEvent()) |event| {
-            switch (event) {
-                .key_press => |key| if (key.matches('c', .{ .ctrl = true })) return,
-                .winsize => |ws| try vx.resize(alloc, tty.writer(), ws),
-            }
-        }
-
-        const win = vx.window();
-        win.clear();
-
-        win.fill(.{ .style = .{ .bg = .{ .rgb = .{ 0, 0, 0 } }, .fg = .{ .rgba = .{ 0, 0, 0, 255 } } } });
-
-        var color = fg;
-        color[3] = pct;
-
-        const style: vaxis.Style = .{ .fg = .{ .rgba = color }, .bg = .{ .rgba = bg } };
-
-        const segment: vaxis.Segment = .{
-            .text = vaxis.logo,
-            .style = style,
-        };
-
-        const center = vaxis.widgets.alignment.center(win, 28, 4);
-        _ = center.printSegment(segment, .{ .wrap = .grapheme });
-
-        try vx.render(tty.writer());
-
-        switch (dir) {
-            .up => {
-                pct += 1;
-                if (pct == 255) dir = .down;
-            },
-            .down => {
-                pct -= 1;
-                if (pct == 0) dir = .up;
-            },
-        }
-    }
+    app.run() catch |err| {
+        log.err("App exist with err: {}", .{err});
+    };
 }
