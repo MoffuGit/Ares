255f497111c16684448c61b9512454855ca4cff7
MoffuGit <moffu.by@gmail.com>
2026-02-24 21:36:46 -0600

    WIP

--- src/core/mod.zig
+++ src/core/mod.zig
@@ -2,6 +2,7 @@ const global = @import("global.zig");
 const alloc = global.state.alloc;
 
 const Settings = @import("settings/mod.zig");
+const Io = @import("io/mod.zig");
 
 export fn init_state() void {
     global.state.init();
@@ -17,4 +18,16 @@ export fn destroySettings(settings: *Settings) void {
 
 export fn loadSettings(settings: *Settings, path: []const u8) !void {
     try settings.load(path);
+}
+
+export fn createIo() !*Io {
+    return try Io.create(alloc);
+}
+
+//NOTE:
+//this needs to be ffi compatible
+// export fn readFile(io: *Io, abs_path, userdata, callback) !void {}
+
+export fn destroyIo(io: *Io) void {
+    io.destroy();
 }
