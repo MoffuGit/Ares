2448539d39db7c39d581fa292a5246d2bd23ab55
MoffuGit <moffu.by@gmail.com>
2026-03-14 18:07:24 -0600

    WIP

--- packages/app/tui/src/app.ts
+++ packages/app/tui/src/app.ts
@@ -1,5 +1,4 @@
 import { CoreApp } from "@ares/shared/core";
-import { log } from "../../../tui/solid/src/utils";
 
 export class TuiApp extends CoreApp {
     constructor(settingsPath: string, projectPath: string, libPath?: string,) {
@@ -13,9 +12,6 @@ export class TuiApp extends CoreApp {
         this.on("filetreeUpdate", () => this.events.emit("filetreeUpdate"));
         this.on("settingsUpdate", () => this.events.emit("settingsUpdate"));
         this.on("themeUpdate", () => this.events.emit("themeUpdate"));
-        this.on("filetreeUpdate", () => {
-            log("filetreeUpdate")
-        })
         setInterval(() => {
             this.drainMailbox()
         }, 100);
