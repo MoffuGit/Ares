0829b1e0a7f80bd4cc880bb76098a36667a06450
MoffuGit <moffu.by@gmail.com>
2026-03-16 19:39:40 -0600

    WIP

--- packages/app/tui/src/app.ts
+++ packages/app/tui/src/app.ts
@@ -8,12 +8,7 @@ export class TuiApp extends CoreApp {
         this.openProject(projectPath);
     }
 
-    start() {
-        this.on("filetreeUpdate", () => this.events.emit("filetreeUpdate"));
-        this.on("settingsUpdate", () => this.events.emit("settingsUpdate"));
-        this.on("themeUpdate", () => this.events.emit("themeUpdate"));
-        this.on("modeUpdate", () => this.events.emit("modeUpdate"));
-        this.on("keymapsUpdate", () => this.events.emit("keymapsUpdate"));
+    override start() {
         setInterval(() => {
             this.drainMailbox()
         }, 100);
