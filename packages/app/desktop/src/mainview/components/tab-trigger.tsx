fa3da20a377acc0db0a6655a24ad36c9998884dd
MoffuGit <moffu.by@gmail.com>
2026-03-25 22:12:19 -0600

    WIP

--- packages/app/desktop/src/mainview/components/tab-trigger.tsx
+++ packages/app/desktop/src/mainview/components/tab-trigger.tsx
@@ -23,11 +23,12 @@ export function TabTrigger({ tab }: TabContentProps) {
             <span className="truncate">{tab.name}</span>
             <Button
                 render={({ children, className }) => {
-                    return <div className={className}>{children}</div>
+                    return <div className={className} 
+                        onClick={(e) => { e.stopPropagation(); closeTab(tab.id); }}
+                    >{children}</div>
                 }}
                 size="icon-xs"
                 className="!pointer-events-auto absolute right-0.5 size-5 top-1/2 transition-none -translate-y-1/2 group-hover/tab-trigger:opacity-100 opacity-0 bg-muted hover:bg-background hover:text-foreground"
-                onClick={(e) => { e.stopPropagation(); closeTab(tab.id); }}
             >
                 <X />
             </Button>
