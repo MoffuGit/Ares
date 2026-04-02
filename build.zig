28e1991b9df05aa747e767e83dbc22e3b8043a16
MoffuGit <moffu.by@gmail.com>
2026-04-01 19:05:15 -0600

    WIP: moved tui to another repo

--- build.zig
+++ build.zig
@@ -1,11 +1,9 @@
 const std = @import("std");
-const yoga_sources = @import("yoga.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const vaxis_dep = b.dependency("vaxis", .{ .target = target, .optimize = optimize });
     const xev_dep = b.dependency("libxev", .{ .target = target, .optimize = optimize });
     const ghostty_dep = b.dependency("ghostty", .{ .target = target, .optimize = optimize });
     const objc_dep = b.dependency("zig_objc", .{
@@ -64,39 +62,6 @@ pub fn build(b: *std.Build) void {
     desktop_step.dependOn(core_step);
     desktop_step.dependOn(&desktop_bun.step);
 
-    const yoga_lib = buildYogaLib(b, target, optimize);
-    const yoga_mod = buildYogaModule(b, target, optimize);
-
-    const tui_lib_mod = b.createModule(.{
-        .root_source_file = b.path("tui/lib.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    tui_lib_mod.addImport("datastruct", datastruct);
-    tui_lib_mod.addImport("xev", xev_dep.module("xev"));
-    tui_lib_mod.addImport("vaxis", vaxis_dep.module("vaxis"));
-    tui_lib_mod.addImport("yoga", yoga_mod);
-
-    const tui_lib = b.addLibrary(.{
-        .name = "tui",
-        .root_module = tui_lib_mod,
-        .linkage = .dynamic,
-    });
-    tui_lib.linkLibrary(yoga_lib);
-    const tui_install = b.addInstallArtifact(tui_lib, .{});
-
-    const tui_lib_step = b.step("tui-lib", "Build tui lib and run the opentui application");
-    tui_lib_step.dependOn(&tui_lib.step);
-    tui_lib_step.dependOn(&tui_install.step);
-
-    const tui = b.addSystemCommand(&.{ "bun", "run", "dev" });
-    tui.setCwd(b.path("packages/app/tui"));
-
-    const tui_step = b.step("tui", "Build tui lib and run the opentui application");
-    tui_step.dependOn(&tui_install.step);
-    tui_step.dependOn(&tui_lib.step);
-    tui_step.dependOn(&tui.step);
-
     const test_filter = b.option([]const u8, "test-filter", "Filter for tests");
 
     const test_core = b.createModule(.{
@@ -115,108 +80,22 @@ pub fn build(b: *std.Build) void {
     test_core.addImport("datastruct", datastruct);
     test_core.addImport("objc", objc_dep.module("objc"));
 
-    const test_tui_core = b.createModule(.{
-        .root_source_file = b.path("tui/test.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    test_tui_core.addImport("xev", xev_dep.module("xev"));
-    test_tui_core.addImport("datastruct", datastruct);
-    test_tui_core.addImport("yoga", yoga_mod);
-    test_tui_core.addImport("vaxis", vaxis_dep.module("vaxis"));
-    test_tui_core.linkLibrary(yoga_lib);
-
     const test_core_exe = b.addTest(.{
         .name = "test-core",
         .root_module = test_core,
         .filters = if (test_filter) |f| &.{f} else &.{},
     });
 
-    const test_tui_core_exe = b.addTest(.{
-        .name = "test-tui-core",
-        .root_module = test_tui_core,
-        .filters = if (test_filter) |f| &.{f} else &.{},
-    });
-
     const test_core_run = b.addRunArtifact(test_core_exe);
-    const test_tui_core_run = b.addRunArtifact(test_tui_core_exe);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&test_core_run.step);
-    test_step.dependOn(&test_tui_core_run.step);
 
     const bun_core_test = b.addSystemCommand(&.{ "bun", "test" });
     bun_core_test.setCwd(b.path("packages/core"));
     bun_core_test.step.dependOn(&core_lib.step);
     bun_core_test.step.dependOn(&lib_install.step);
 
-    const bun_tui_core_test = b.addSystemCommand(&.{ "bun", "test" });
-    bun_tui_core_test.setCwd(b.path("packages/tui"));
-    bun_tui_core_test.step.dependOn(&tui_lib.step);
-    bun_tui_core_test.step.dependOn(&tui_install.step);
-
     test_step.dependOn(&bun_core_test.step);
-    test_step.dependOn(&bun_tui_core_test.step);
-}
-
-fn buildYogaLib(
-    b: *std.Build,
-    target: std.Build.ResolvedTarget,
-    optimize: std.builtin.OptimizeMode,
-) *std.Build.Step.Compile {
-    const yoga_lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "yoga",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
-    yoga_lib.linkLibCpp();
-    yoga_lib.addIncludePath(b.path("yoga"));
-
-    for (yoga_sources.yoga_cpps) |src| {
-        yoga_lib.addCSourceFile(.{
-            .file = b.path(src),
-            .flags = &.{"-std=c++20"},
-        });
-    }
-
-    ensureYogaCloned(b, &yoga_lib.step);
-
-    return yoga_lib;
-}
-
-fn buildYogaModule(
-    b: *std.Build,
-    target: std.Build.ResolvedTarget,
-    optimize: std.builtin.OptimizeMode,
-) *std.Build.Module {
-    const translate_c = b.addTranslateC(.{
-        .root_source_file = b.path("yoga/yoga/yoga.h"),
-        .target = target,
-        .optimize = optimize,
-    });
-    translate_c.addIncludePath(b.path("yoga"));
-
-    return b.createModule(.{
-        .root_source_file = translate_c.getOutput(),
-        .target = target,
-        .optimize = optimize,
-    });
-}
-
-fn ensureYogaCloned(b: *std.Build, dependent_step: *std.Build.Step) void {
-    const cwd = std.fs.cwd();
-    cwd.access("yoga", .{}) catch |err| {
-        if (err == error.FileNotFound) {
-            const git_clone = b.addSystemCommand(&.{
-                "git",                                  "clone",
-                "https://github.com/facebook/yoga.git", "--depth=1",
-            });
-            dependent_step.dependOn(&git_clone.step);
-        }
-    };
 }
 /// A zig build step that compiles a set of ".metal" files into a
 /// ".metallib" file.
--- tui/test.zig
+++
@@ -1,8 +0,0 @@
-test {
-    _ = @import("ClipRect.zig");
-    _ = @import("Buffer.zig");
-    _ = @import("window/element/mod.zig");
-    _ = @import("window/HitGrid.zig");
-    _ = @import("mutations/mod.zig");
-    _ = @import("mutations/Parser.zig");
-}
