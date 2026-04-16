988a02c49a4194c8cbf35caf33e7605a05eeeb2c
MoffuGit <moffu.by@gmail.com>
2026-04-15 20:46:10 -0600

    WIP

--- packages/app/src/mainview/components/sidebar-tabs.tsx
+++ packages/app/src/mainview/components/sidebar-tabs.tsx
@@ -21,7 +21,7 @@ export function SidebarTabs() {
         <>
             <SidebarContent>
                 <SidebarGroup>
-                    <SidebarGroupLabel className="font-instrument mb-3 px-1 h-auto text-2xl font-normal leading-none antialiased text-sidebar-foreground">
+                    <SidebarGroupLabel>
                         {project?.name}
                     </SidebarGroupLabel>
                     <SidebarGroupContent>
