a8e43fffbf353256d98cf05e3b9769c4cbdb9909
MoffuGit <moffu.by@gmail.com>
2026-04-15 18:02:03 -0600

    WIP

--- packages/app/src/mainview/components/ui/sidebar.tsx
+++ packages/app/src/mainview/components/ui/sidebar.tsx
@@ -316,12 +316,13 @@ function SidebarRail({ className, ...props }: React.ComponentProps<"button">) {
             onPointerDown={handlePointerDown}
             title="Toggle Sidebar"
             className={cn(
-                "absolute inset-y-0 z-20 hidden w-1 transition-all ease-linear group-data-[side=left]:-right-1 group-data-[side=right]:left-0 after:absolute after:my-3 after:inset-y-0 after:start-1/2 after:w-[2px] hover:after:bg-sidebar-border sm:flex ltr:-translate-x-1/2 rtl:-translate-x-1/2",
+                "absolute inset-y-0 z-20 hidden w-1  group-data-[side=left]:-right-0.5 group-data-[side=right]:left-0 after:absolute after:my-3 after:inset-y-0 after:start-1/2 after:w-[2px] opacity-0 hover:opacity-100 sm:flex ltr:-translate-x-1/2 rtl:-translate-x-1/2",
                 "in-data-[side=left]:cursor-col-resize in-data-[side=right]:cursor-col-resize",
                 "[[data-side=left][data-state=collapsed]_&]:cursor-e-resize [[data-side=right][data-state=collapsed]_&]:cursor-w-resize",
                 "group-data-[collapsible=offcanvas]:translate-x-0 group-data-[collapsible=offcanvas]:after:left-full hover:group-data-[collapsible=offcanvas]:bg-sidebar",
-                "[[data-side=left][data-collapsible=offcanvas]_&]:-right-0",
+                "[[data-side=left][data-collapsible=offcanvas]_&]:right-0.5",
                 "[[data-side=right][data-collapsible=offcanvas]_&]:-left-2",
+                "after:bg-sidebar-border/90",
                 className
             )}
             {...props}
