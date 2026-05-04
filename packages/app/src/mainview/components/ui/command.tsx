b35d78e0c2eb6281e39c2e489993ebc20b5c225d
MoffuGit <moffu.by@gmail.com>
2026-05-04 15:54:28 -0600

    WIP

--- packages/app/src/mainview/components/ui/command.tsx
+++ packages/app/src/mainview/components/ui/command.tsx
@@ -52,7 +52,7 @@ function CommandDialog({
             </DialogHeader>
             <DialogContent
                 className={cn(
-                    "top-1/7 translate-y-0 overflow-hidden rounded-2xl! p-0 bg-popover/70 backdrop-blur-2xl border-[1px] border-border/80 shadow-cmd ring-0",
+                    "top-1/7 translate-y-0 overflow-hidden rounded-xl! p-0 bg-popover/70 backdrop-blur-2xl border-[1px] border-border/80 shadow-cmd ring-0",
                     className
                 )}
                 showCloseButton={showCloseButton}
@@ -120,7 +120,7 @@ function CommandGroup({
         <CommandPrimitive.Group
             data-slot="command-group"
             className={cn(
-                "overflow-hidden p-1 text-foreground **:[[cmdk-group-heading]]:px-3 **:[[cmdk-group-heading]]:pt-1 **:[[cmdk-group-heading]]:pb-2 **:[[cmdk-group-heading]]:text-xs **:[[cmdk-group-heading]]:font-medium **:[[cmdk-group-heading]]:text-muted-foreground",
+                "overflow-hidden p-1 text-foreground **:[[cmdk-group-heading]]:px-2 **:[[cmdk-group-heading]]:pb-2 **:[[cmdk-group-heading]]:text-xs **:[[cmdk-group-heading]]:font-medium **:[[cmdk-group-heading]]:text-muted-foreground",
                 className
             )}
             {...props}
@@ -150,7 +150,7 @@ function CommandItem({
         <CommandPrimitive.Item
             data-slot="command-item"
             className={cn(
-                "group/command-item relative flex cursor-default items-center gap-2 rounded-sm px-4 py-2 text-[13px] font-normal outline-hidden select-none in-data-[slot=dialog-content]:rounded-lg! data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 data-selected:bg-muted data-selected:text-foreground [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 data-selected:*:[svg]:text-foreground",
+                "group/command-item relative flex cursor-default items-center gap-2 rounded-sm p-3 text-[13px] font-normal outline-hidden select-none in-data-[slot=dialog-content]:rounded-lg! data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50 data-selected:bg-background data-selected:text-foreground [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 data-selected:*:[svg]:text-foreground",
                 className
             )}
             {...props}
