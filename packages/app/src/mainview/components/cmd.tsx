a3e676b3c258deb95481dfac391594344f61dcd6
MoffuGit <moffu.by@gmail.com>
2026-05-04 15:56:50 -0600

    WIP

--- packages/app/src/mainview/components/cmd.tsx
+++ packages/app/src/mainview/components/cmd.tsx
@@ -1,5 +1,5 @@
 import { useShallow } from "zustand/react/shallow";
-import { Command, CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandShortcut } from "./ui/command";
+import { Command, CommandDialog, CommandGroup, CommandInput, CommandItem, CommandList, CommandShortcut } from "./ui/command";
 import { findCmdSequence, selectAvailableCmds, useAppStore } from "@/lib/app";
 
 export function Cmd() {
@@ -14,7 +14,6 @@ export function Cmd() {
             <Command>
                 <CommandInput placeholder="Type a command or search..." />
                 <CommandList>
-                    <CommandEmpty>No results found.</CommandEmpty>
                     <CommandGroup heading="Commands">
                         {cmds.map(({ cmd, handler }) => {
                             const sequence = findCmdSequence(keymaps, cmd, mode);
