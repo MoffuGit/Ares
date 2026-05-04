5f03531c8d68567651489993f1ad49f1b430c5b2
MoffuGit <moffu.by@gmail.com>
2026-05-03 20:33:39 -0600

    WIP: cmd top position

--- packages/app/src/mainview/components/ui/command.tsx
+++ packages/app/src/mainview/components/ui/command.tsx
@@ -53,7 +53,7 @@ function CommandDialog({
             </DialogHeader>
             <DialogContent
                 className={cn(
-                    "top-1/3 translate-y-0 overflow-hidden rounded-xl! p-0",
+                    "top-1/7 translate-y-0 overflow-hidden rounded-xl! p-0",
                     className
                 )}
                 showCloseButton={showCloseButton}
