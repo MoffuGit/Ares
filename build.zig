4ab190555257488d3001157bcb9de843de8cef8c
MoffuGit <moffu.by@gmail.com>
2026-05-19 11:50:24 -0600

    WIP

--- build.zig
+++ build.zig
@@ -12,6 +12,7 @@ pub fn build(b: *std.Build) void {
         "Profiling level: none, general, or deep. Tests always use none; bench defaults to general.",
     );
 
+    // exe: any level, default none.
     const exe = b.addExecutable(.{
         .name = "Odyssey",
         .root_module = rootModule(b, target, optimize, "src/main.zig", requested orelse .none, false),
@@ -23,9 +24,12 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| run_cmd.addArgs(args);
     b.step("run", "Run the app").dependOn(&run_cmd.step);
 
+    const clone_chromium = cloneChromiumStep(b);
+
     const exe_tests = b.addTest(.{
-        .root_module = rootModule(b, target, optimize, "src/test.zig", .none, false),
+        .root_module = rootModule(b, target, optimize, "src/main.zig", .none, false),
     });
+    exe_tests.step.dependOn(clone_chromium);
     b.step("test", "Run tests").dependOn(&b.addRunArtifact(exe_tests).step);
 
     const bench_level: ProfileLevel = switch (requested orelse .general) {
@@ -35,6 +39,8 @@ pub fn build(b: *std.Build) void {
     const bench_tests = b.addTest(.{
         .root_module = rootModule(b, target, optimize, "src/bench.zig", bench_level, true),
     });
+
+    bench_tests.step.dependOn(clone_chromium);
     const run_bench = b.addRunArtifact(bench_tests);
     run_bench.argv.shrinkRetainingCapacity(1);
     run_bench.stdio = .inherit;
@@ -66,4 +72,54 @@ fn rootModule(
         .optimize = optimize,
         .imports = &.{.{ .name = "prof", .module = prof_mod }},
     });
+}
+
+const chromium_dir = "testdata/chromium";
+const chromium_url = "https://github.com/chromium/chromium.git";
+
+fn cloneChromiumStep(b: *std.Build) *std.Build.Step {
+    const step = b.allocator.create(std.Build.Step) catch @panic("OOM");
+    step.* = std.Build.Step.init(.{
+        .id = .custom,
+        .name = "clone chromium",
+        .owner = b,
+        .makeFn = cloneChromiumMake,
+    });
+    return step;
+}
+
+fn cloneChromiumMake(step: *std.Build.Step, opts: std.Build.Step.MakeOptions) anyerror!void {
+    const b = step.owner;
+    const io = b.graph.io;
+    const dest = b.pathFromRoot(chromium_dir);
+    const git_dir = b.pathJoin(&.{ dest, ".git" });
+
+    if (std.Io.Dir.accessAbsolute(io, git_dir, .{})) |_| return else |_| {}
+
+    std.Io.Dir.cwd().createDirPath(io, b.pathFromRoot("testdata")) catch |e|
+        return step.fail("unable to create testdata dir: {s}", .{@errorName(e)});
+
+    var node = opts.progress_node.start("git clone chromium", 0);
+    defer node.end();
+
+    var child = try std.process.spawn(io, .{
+        .argv = &.{
+            "git",                "clone",
+            "--depth",            "1",
+            "--filter=blob:none", chromium_url,
+            dest,
+        },
+        .stdin = .pipe,
+        .stdout = .pipe,
+        .stderr = .pipe,
+    });
+
+    const term = child.wait(io) catch |e|
+        return step.fail("failed to spawn git: {s}", .{@errorName(e)});
+
+    switch (term) {
+        .exited => |code| if (code != 0)
+            return step.fail("git clone exited with code {d}", .{code}),
+        else => return step.fail("git clone terminated unexpectedly", .{}),
+    }
 }
