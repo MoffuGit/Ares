88c8fdfaab3046e8ef1744e208f3d9fd391d1d70
MoffuGit <moffu.by@gmail.com>
2026-05-26 10:22:52 -0600

    WIP

---
+++ Odyssey/Odyssey/OdysseyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdysseyApp.swift
+//  Odyssey
+//
+//  Created by Adrian Hess on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct OdysseyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
