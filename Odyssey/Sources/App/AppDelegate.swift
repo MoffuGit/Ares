142837eb12d1e24bf30161c8dc87d075fe779569
MoffuGit <moffu.by@gmail.com>
2026-06-01 11:52:38 -0600

    WIP

--- Odyssey/Sources/App/AppDelegate.swift
+++ Odyssey/Sources/App/AppDelegate.swift
@@ -9,25 +9,15 @@ import Cocoa
 import OdysseyKit
 import os
 
-@main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
     private var app: Odyssey.App!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let result = odyssey_init(0, nil)
-
-        guard result == 0 else {
-            Odyssey.logger.critical("odyssey_init failed: \(result)")
-            NSApp.terminate(self)
-            return
-        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        app = nil
-        odyssey_deinit()
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
