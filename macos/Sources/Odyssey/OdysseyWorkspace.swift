e9fcdef41d14de02ddf43584ce73a1fe17376815
MoffuGit <moffu.by@gmail.com>
2026-07-02 11:01:07 -0600

    WIP: Odyssey Workspace

--- macos/Sources/Odyssey/OdysseyWorkspace.swift
+++ macos/Sources/Odyssey/OdysseyWorkspace.swift
@@ -8,26 +8,34 @@ import OdysseyKit
 import os
 
 extension Odyssey {
-    // class Workspace {
-    //     var app: Odyssey.App
-    //     var workspace: odyssey_workspace_t? {
-    //         didSet {
-    //             guard let old = oldValue else { return }
-    //             odyssey_workspace_free( old)
-    //         }
-    //     }
-    //
-    //     init(app: Odyssey.App) {
-    //         self.app = app
-    //         guard let workspace = odyssey_workspace_new() else {
-    //             logger.critical("odyssey_workspace_new failed")
-    //             return
-    //         }
-    //         self.workspace = workspace
-    //     }
-    //
-    //     deinit {
-    //         self.workspace = nil
-    //     }
-    // }
+    class Workspace {
+        var app: Odyssey.App
+        var workspace: odyssey_workspace_t? {
+            didSet {
+                guard let old = oldValue else { return }
+                odyssey_drop_entity(old)
+            }
+        }
+
+        init(app: Odyssey.App) {
+            self.app = app
+
+            guard let app = app.app else {
+                logger.critical("odyssey_workspace_new failed: app is nil")
+                return
+            }
+
+            let creation = odyssey_workspace_new(app)
+            guard creation.valid else {
+                logger.critical("odyssey_workspace_new failed")
+                return
+            }
+
+            self.workspace = creation.entity
+        }
+
+        deinit {
+            self.workspace = nil
+        }
+    }
 }
