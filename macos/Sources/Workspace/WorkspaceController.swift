761615bf966f2d10665afbb8ba88df7c02f1ecea
MoffuGit <moffu.by@gmail.com>
2026-07-17 16:39:17 -0600

    WIP

--- macos/Sources/Workspace/WorkspaceController.swift
+++ macos/Sources/Workspace/WorkspaceController.swift
@@ -11,42 +11,45 @@ class WorkspaceController: NSWindowController, NSWindowDelegate, WorkspaceDelega
     let paths: [String]
     private let toolBar = WorkspaceToolBar()
     private let statusBar = WorkspaceStatusBar()
-    private let content = NSView()
     private let docks: WorkspaceDocks
+    private let content = NSStackView()
     internal let workspace: Odyssey.Workspace
     private weak var appDelegate: AppDelegate?
-    
-    convenience init(delegate: AppDelegate, session: Odyssey.Session, path: String, rect: NSRect? = nil) {
+
+    convenience init(
+        delegate: AppDelegate, session: Odyssey.Session, path: String, rect: NSRect? = nil
+    ) {
         self.init(delegate: delegate, session: session, paths: [path], rect: rect)
     }
-    
+
     init(delegate: AppDelegate, session: Odyssey.Session, paths: [String], rect: NSRect? = nil) {
         self.paths = Odyssey.Workspace.normalizedPaths(paths)
         self.workspace = Odyssey.Workspace(app: delegate.app, session: session, paths: self.paths)
         self.appDelegate = delegate
-        self.docks = WorkspaceDocks(center: content);
-        
-        super.init(window: WorkspaceWindow(rect ?? NSRect(x: 0, y: 0, width: 1200, height: 800)))
-        
-        let content = NSStackView()
+        self.docks = WorkspaceDocks(center: NSView())
+
         content.orientation = .vertical
         content.spacing = 0
         content.wantsLayer = true
         content.layer?.masksToBounds = true
         content.layer?.cornerRadius = 16
         content.layer?.backgroundColor = .white
+
         content.addArrangedSubview(toolBar)
         content.addArrangedSubview(docks)
         content.addArrangedSubview(statusBar)
-        content.needsDisplay = true
-        window?.contentView = content
+
+        let window = WorkspaceWindow(rect ?? NSRect(x: 0, y: 0, width: 1200, height: 800))
+        super.init(window: window)
+        window.contentView = content
+        window.delegate = self
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     func serializedMetadata() -> Odyssey.SerializedWorkspace? {
         let id = workspace.Id
         guard id >= 0 else { return nil }
@@ -57,23 +60,27 @@ class WorkspaceController: NSWindowController, NSWindowDelegate, WorkspaceDelega
             rect: window!.frame
         )
     }
-    
+
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        return true
+    }
+
     func windowWillClose(_ notification: Notification) {
         appDelegate?.workspaceWindowWillClose(self)
     }
-    
+
     func toggleLeftDock() {
-        docks.toggleCollapsed(.left);
+        docks.toggleCollapsed(.left)
     }
-    
+
     func toggleRightDock() {
-        docks.toggleCollapsed(.right);
+        docks.toggleCollapsed(.right)
     }
-    
+
     func markForRestoration() {
         workspace.markForRestoration()
     }
-    
+
     func saveBounds() {
         workspace.setBounds(window: window!.frame, leftDock: 0.0, rightDock: 0.0)
     }
@@ -81,7 +88,7 @@ class WorkspaceController: NSWindowController, NSWindowDelegate, WorkspaceDelega
 
 private class WorkspaceWindow: NSWindow {
     override var canBecomeKey: Bool { true }
-    
+
     init(_ rect: NSRect) {
         super.init(
             contentRect: rect,
@@ -89,12 +96,10 @@ private class WorkspaceWindow: NSWindow {
             backing: .buffered,
             defer: false
         )
-        
-        self.isReleasedWhenClosed = true
+
         self.isRestorable = false
-        self.backgroundColor = .clear
+        self.backgroundColor = .black
         self.titlebarAppearsTransparent = true
-        self.titleVisibility = .hidden
         self.contentMinSize = NSSize(width: 480, height: 300)
     }
 }
