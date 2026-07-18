bccf064ba06ad4b6b3ba26b968ccf75491ef4756
MoffuGit <moffu.by@gmail.com>
2026-07-18 11:12:44 -0600

    WIP

--- src/project/worktree_store.zig
+++ src/project/worktree_store.zig
@@ -1,12 +1,13 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const App = @import("../app.zig");
+const Entity = App.Entity;
 const Context = App.Context;
 const Worktree = @import("../worktree.zig");
 
 const WorktreeStore = @This();
 
-worktrees: std.AutoHashMap(u8, Worktree),
+worktrees: std.AutoHashMap(u8, Entity(Worktree)),
 next_id: u8,
 
 pub fn init(self: *WorktreeStore, _: Context(WorktreeStore), arena: Allocator) !void {
@@ -14,6 +15,13 @@ pub fn init(self: *WorktreeStore, _: Context(WorktreeStore), arena: Allocator) !
         .next_id = 0,
         .worktrees = .init(arena),
     };
+}
+
+pub fn drop(self: *WorktreeStore) void {
+    var iter = self.worktrees.iterator();
+    while (iter.next()) |entry| {
+        entry.value_ptr.drop();
+    }
 }
 
 pub fn deinit(self: *WorktreeStore) void {
