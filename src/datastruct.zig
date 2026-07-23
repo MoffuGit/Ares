03dbecf383a7d259067b9d8a2a516bfa6e86a632
MoffuGit <moffu.by@gmail.com>
2026-07-23 14:33:43 -0600

    WIP: DoublyLinkedList

--- src/datastruct.zig
+++ src/datastruct.zig
@@ -6,8 +6,10 @@ pub const queue = @import("datastruct/queue.zig");
 pub const slotmap = @import("datastruct/slotmap.zig");
 pub const heap = @import("datastruct/heap.zig");
 pub const stealing = @import("datastruct/stealing_queue.zig");
+pub const doubly_linked_list = @import("datastruct/doubly_linked_list.zig");
 
 test {
+    _ = doubly_linked_list;
     _ = heap;
     _ = multi_mpsc;
     _ = multi_queue;
