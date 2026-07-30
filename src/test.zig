8ff29417ad3b5664ca5e8d3745a9cdf4c899c8d6
MoffuGit <moffu.by@gmail.com>
2026-07-30 11:44:05 -0600

    WIP

--- src/test.zig
+++ src/test.zig
@@ -1,12 +1,13 @@
 const App = @import("app.zig");
+const bench = @import("bench.zig");
 const Buffer = @import("buffer.zig");
-const patch = @import("patch.zig");
 const chunk_pool = @import("chunk_pool.zig");
 const chunked_path = @import("chunked_path.zig");
 const datastruct = @import("datastruct.zig");
 const entity = @import("entity.zig");
 const executor = @import("executor.zig");
 const Loop = @import("loop.zig");
+const patch = @import("patch.zig");
 const Project = @import("project.zig");
 const WorktreeStore = @import("project/worktree_store.zig");
 const Session = @import("session.zig");
@@ -14,7 +15,6 @@ const subscription = @import("subscription.zig");
 const tasks = @import("tasks.zig");
 const Workspace = @import("workspace.zig");
 const Worktree = @import("worktree.zig");
-const bench = @import("bench.zig");
 
 test {
     _ = bench;
