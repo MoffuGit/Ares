8a604565824817fd2f94cb1cf8b094552d329a5d
MoffuGit <moffu.by@gmail.com>
2026-07-29 20:15:20 -0600

    WIP: Buffer

--- src/math.zig
+++ src/math.zig
@@ -1,7 +1,15 @@
+const std = @import("std");
+const debug = std.debug;
+const assert = debug.assert;
+
 ///[min, max) range
 pub const Rngu64 = struct {
     min: u64,
     max: u64,
+
+    pub fn new(min: u64, max: u64) Rngu64 {
+        return .{ .min = min, .max = max };
+    }
 
     pub fn contains(self: *const @This(), int: u64) bool {
         return int >= self.min and int < self.max;
@@ -10,4 +18,17 @@ pub const Rngu64 = struct {
     pub fn dim(self: *const @This()) u64 {
         return if (self.max > self.min) self.max - self.min else 0;
     }
+
+    pub fn empty(self: *const @This()) bool {
+        return self.min >= self.max;
+    }
+
+    pub fn intersect(a: Rngu64, b: Rngu64) Rngu64 {
+        return .new(@max(a.min, b.min), @min(a.max, b.max));
+    }
+};
+
+pub const Rngu64Node = struct {
+    range: Rngu64,
+    next: ?*Rngu64Node = null,
 };
