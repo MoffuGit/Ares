ee47a2ae22a39c93cf0311ae9470879786c492a0
MoffuGit <moffu.by@gmail.com>
2026-08-12 23:02:39 -0600

    WIP

--- macos/Sources/Workspace/WorkspaceDelegate.swift
+++
@@ -1,9 +0,0 @@
-@MainActor
-protocol WorkspaceDelegate {
-    var workspace: Odyssey.Workspace { get }
-
-    func toggleLeftDock()
-    func toggleRightDock()
-    func markForRestoration()
-    func saveBounds()
-}
