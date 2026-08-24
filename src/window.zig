b550bf91b5ce88a1b9b5263d828d7fc429be56e6
MoffuGit <moffu.by@gmail.com>
2026-08-23 19:48:30 -0600

    WIP

--- src/window.zig
+++ src/window.zig
@@ -1,20 +1,22 @@
 const builtin = @import("builtin");
 const rgfw = @import("rgfw");
 
-pub const Window = if (builtin.is_test) TestWindow else rgfw.Window;
+pub const Window = window: {
+    if (!builtin.is_test) break :window rgfw.Window;
 
-const TestWindow = struct {
-    pub fn init(
-        _: *@This(),
-        _: [:0]const u8,
-        _: i32,
-        _: i32,
-        _: i32,
-        _: i32,
-        _: rgfw.Window.Flags,
-    ) !void {}
+    break :window struct {
+        pub fn init(
+            _: *@This(),
+            _: [:0]const u8,
+            _: i32,
+            _: i32,
+            _: i32,
+            _: i32,
+            _: rgfw.Window.Flags,
+        ) !void {}
 
-    pub fn deinit(self: *@This()) void {
-        _ = self;
-    }
+        pub fn deinit(self: *@This()) void {
+            _ = self;
+        }
+    };
 };
