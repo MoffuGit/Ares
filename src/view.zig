d2dfa1b92c6b3d8b265fdc4faedc4b0484766b26
MoffuGit <moffu.by@gmail.com>
2026-09-01 15:29:14 -0600

    WIP

--- src/view.zig
+++ src/view.zig
@@ -306,8 +306,8 @@ const Axis = enum(u1) { x = 0, y = 1 };
 const Size = union(enum) {
     const zero: Size = .{ .fixed = 0 };
 
-    fit: void,
-    grow: void,
+    fit,
+    grow,
     fixed: f32,
     percent: f32,
 };
